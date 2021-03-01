620d506ece6c4527bd9323b0ae75bb938208e573
Asama Masakul <asama@pirsquare.net>
2021-03-02 02:00:24 +0700

    delete comment crypto service

--- src/modules/utils/crypto.service.ts
+++ src/modules/utils/crypto.service.ts
@@ -22,41 +22,4 @@ export class CryptoService {
       expiresIn: exipration || +process.env.EXPIRATION_TIME * 60 * 60 * 24,
     });
   }
-
-  // async encryptData(data) {
-  //   return crypto.AES.encrypt(
-  //     JSON.stringify(data),
-  //     process.env.MAIL_TOKEN_SECRET,
-  //   ).toString();
-  // }
-
-  // async decryptData(token) {
-  //   const bytes = crypto.AES.decrypt(token, process.env.MAIL_TOKEN_SECRET);
-  //   return JSON.parse(bytes.toString(crypto.enc.Utf8));
-  // }
-
-  // async randomToken(len = 20, encoding: any = 'hex') {
-  //   return randomBytes(len).toString(encoding);
-  // }
-
-  // iv = 'e539ce88efb0ca0c';
-  // algorithm = 'aes-256-ctr';
-  // secretkey = 'ae539ce88efb00c48cbf883bc3c5e11b';
-  // encryptIv = (text, encoding: any = 'hex') => {
-  //   let cipher = createCipheriv(this.algorithm, this.secretkey, this.iv);
-  //   let crypted = cipher.update(text.toString(), 'utf8', encoding);
-  //   crypted += cipher.final(encoding);
-  //   return crypted;
-  // };
-
-  // decryptIv = (text, encoding: any = 'hex') => {
-  //   try {
-  //     let decipher = createDecipheriv(this.algorithm, this.secretkey, this.iv);
-  //     let dec = decipher.update(text.toString(), encoding, 'utf8');
-  //     dec += decipher.final('utf8');
-  //     return dec;
-  //   } catch (error) {
-  //     return text;
-  //   }
-  // };
 }
